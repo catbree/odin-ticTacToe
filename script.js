d66c2bf3c79bad0bdb704174c600ec634148860b
Hoi Cheng <49464204+catbree@users.noreply.github.com>
2024-03-28 01:43:48 +0800

    minimised global code, placed everything in factories!

--- script.js
+++ script.js
@@ -1,193 +1,142 @@
-//Use player to track marker and scoring
-
-const player1 = createPlayer("player1", "X");
-const player2 = createPlayer("player2", "O");
-
-function createPlayer(name, marker) {
-    const playerName = name;
-    const playerMarker = marker;
-
-    let score = 0;
-    const getScore = () => score;
-    const increaseScore = () => score++;
-
-
-    return { playerName, playerMarker }
-}
-
-  
-
-//Use gameboard to keep track of markers
-
-let gameboard = createGameboard();
-
-function createGameboard() {
-    let gameboard = [];
-    fillGameboardWithCells();
-
-    function createCell(id) {
-        let cellId = id;
-        let cellMarker = "";
-        return { cellId, cellMarker }
-    }
-
-    function fillGameboardWithCells() {
-        for (i=0;i<9;i++) {
-            gameboard[i] = createCell(i);
-        }
-    }
-
-    return gameboard;
-}
-
-// Display controller to initiate interface
-const displayController = (function() {
+const Gameboard = (function() {
+    let cell = [];
     
-    const startGameButton = document.querySelector(".startGameButton");
-    const tableGameboard = document.querySelector(".tableGameboard");
-    const announcement = document.querySelector(".announcement");
-    const gameboardCell = document.querySelectorAll(".gameboardCell");
+    for (i=0;i<9;i++) {
+        cell[i] = createCell(i);
+    }
 
-    gameboardCell.forEach( cell => {
+    function createCell(i) {
+        let index = i;
+        let marker = "";
+        return { index, marker }
+    }
+
+    return { cell }
+})();
+
+const Player = (function() {
+    const player1 = createPlayer("player1", "X");
+    const player2 = createPlayer("player2", "O");
+
+    function createPlayer(string,symbol) {
+        const name = string;
+        const marker = symbol;
+
+        return { name, marker}
+    }
+
+    return { player1, player2 }
+})();
+
+const displayController = (function() {
+   
+    const cells = document.querySelectorAll(".gameboardCell");
+    const announcement = document.querySelector(".announcement");
+
+    cells.forEach( cell => {
         cell.addEventListener("click", (e) => {
-            moderator.playRound(parseInt(e.target.dataset.index),moderator.getCurrentPlayer());
-            
-            for (i=0;i<gameboardCell.length;i++) {
-                    console.log(`updated!`)
-                    gameboardCell[i].textContent = gameboard[i].cellMarker; 
-            }
-            
+            gameController.playRound(parseInt(e.target.dataset.index),gameController.getCurrentPlayer());
+            updateGameboard();
         })
     })
 
-    startGameButton.addEventListener("click", () => {
-        startGameButton.style.display = "none";
-    })
-
-    const updateGameboard = () => {
-        for (i=0;i<gameboardCell.length;i++) {
-            console.log(`updated!`)
-            gameboardCell.textContent = gameboard[i].cellMarker;
+    const updateGameboard = (e) => {
+        for (let i=0;i<9;i++) {
+            cells[i].textContent = Gameboard.cell[i].marker;
         }
     }
 
-    return { announcement, gameboardCell }
+    return { announcement }
+
 })();
 
-//Use moderator to facilitate gameplay
-
-const moderator = (function () {
+const gameController = (function() {
     let turn = 0;
     let gameOver = false;
 
-    const playRound = (number,currentPlayer) => {
-
-        if (turn === 9) {
-            displayController.announcement.textContent(`It's a draw`);
+    const playRound = (index,currentPlayer) => {
+        if (turn === 8) {
+            displayController.announcement.textContent = `It's a draw`;
             gameOver = true;
             return;
-          }
+        }
 
-        let selectedCell = gameboard[number];
-        console.log(`The selected cell is currently marked as ${selectedCell.cellMarker}`)
-
-        if (selectedCell.cellMarker == "") {
-            console.log(`Marking cell with ${currentPlayer.playerMarker}`)
-            gameboard[number].cellMarker = currentPlayer.playerMarker;
-            console.table(gameboard);
+        if (Gameboard.cell[index].marker == "") {
+            Gameboard.cell[index].marker = currentPlayer.marker;
             checkWinCondition(currentPlayer);
             turn++;
         } else {
             console.log(`This cell is already occupied.`)
         }
+
+        console.table(Gameboard.cell);
+    }
+
+      const getCurrentPlayer = () => {
+        return turn%2 === 0 ? Player.player1 : Player.player2
+    }  
+    
+    function checkWinCondition(currentPlayer) {
+        let x = currentPlayer.marker;
         
-    }
-
-    const getCurrentPlayer = () => {
-        return turn%2 === 0 ? player1 : player2
-    }
-
-    function checkWinCondition(player) {
-        let x = player.playerMarker;
-        
-        if (gameboard[0].cellMarker == x && 
-            gameboard[1].cellMarker == x && 
-            gameboard[2].cellMarker == x) {
-                return announcePlayerWin(player)
+        if (Gameboard.cell[0].marker == x && 
+            Gameboard.cell[1].marker == x && 
+            Gameboard.cell[2].marker == x) {
+                return announcePlayerWin(currentPlayer)
         }
 
-        if (gameboard[3].cellMarker == x && 
-            gameboard[4].cellMarker == x && 
-            gameboard[5].cellMarker == x) {
-                return announcePlayerWin(player)
+        if (Gameboard.cell[3].marker == x && 
+            Gameboard.cell[4].marker == x && 
+            Gameboard.cell[5].marker == x) {
+                return announcePlayerWin(currentPlayer)
         }
 
-        if (gameboard[6].cellMarker == x && 
-            gameboard[7].cellMarker == x && 
-            gameboard[8].cellMarker == x) {
-                return announcePlayerWin(player)
+        if (Gameboard.cell[6].marker == x && 
+            Gameboard.cell[7].marker == x && 
+            Gameboard.cell[8].marker == x) {
+                return announcePlayerWin(currentPlayer)
         }
 
-        if (gameboard[0].cellMarker == x && 
-            gameboard[3].cellMarker == x && 
-            gameboard[6].cellMarker == x) {
-                return announcePlayerWin(player)
+        if (Gameboard.cell[0].marker == x && 
+            Gameboard.cell[3].marker == x && 
+            Gameboard.cell[6].marker == x) {
+                return announcePlayerWin(currentPlayer)
         }
 
-        if (gameboard[1].cellMarker == x && 
-            gameboard[4].cellMarker == x && 
-            gameboard[7].cellMarker == x) {
-                return announcePlayerWin(player)
+        if (Gameboard.cell[1].marker == x && 
+            Gameboard.cell[4].marker == x && 
+            Gameboard.cell[7].marker == x) {
+                return announcePlayerWin(currentPlayer)
         }
 
-        if (gameboard[2].cellMarker == x && 
-            gameboard[5].cellMarker == x && 
-            gameboard[8].cellMarker == x) {
-                return announcePlayerWin(player)
+        if (Gameboard.cell[2].marker == x && 
+            Gameboard.cell[5].marker == x && 
+            Gameboard.cell[8].marker == x) {
+                return announcePlayerWin(currentPlayer)
         }
 
-        if (gameboard[0].cellMarker == x && 
-            gameboard[4].cellMarker == x && 
-            gameboard[8].cellMarker == x) {
-                return announcePlayerWin(player)
+        if (Gameboard.cell[0].marker == x && 
+            Gameboard.cell[4].marker == x && 
+            Gameboard.cell[8].marker == x) {
+                return announcePlayerWin(currentPlayer)
         }
 
-        if (gameboard[6].cellMarker == x && 
-            gameboard[4].cellMarker == x && 
-            gameboard[2].cellMarker == x) {
-                return announcePlayerWin(player)
+        if (Gameboard.cell[6].marker == x && 
+            Gameboard.cell[4].marker == x && 
+            Gameboard.cell[2].marker == x) {
+                return announcePlayerWin(currentPlayer)
         }
-    }
-
-    function checkTieCondition() {
-        let occupiedSpace = 0;
-        let tieCondition = false;
-
-        for (i=0;i<9;i++) {
-            if(gameboard[i].cellMarker!=='') {
-                occupiedSpace++;
-            }
-        }
-        if (occupiedSpace==9) {
-            tieCondition = true;
-            announceTie();
-        } else {
-            occupiedSpace = 0;
-            increaseTurn();
-            playerTurn();
-        }
-
-        return tieCondition
     }
 
     function announcePlayerWin(player) {
-        alert(`${player.playerName} has won!`)
-
+        displayController.announcement.textContent = `${player.name} has won!`;
+        gameOver = true;
     }
 
     function announceTie() {
         alert(`It's a draw!`)
     }
 
-    return { playRound, checkTieCondition, getCurrentPlayer }
+    return { playRound, getCurrentPlayer }
+
 })();
---
+++ script2.js
@@ -0,0 +1,192 @@
+//Use player to track marker and scoring
+function createPlayer(name, marker) {
+    const playerName = name;
+    const playerMarker = marker;
+
+    let score = 0;
+    const getScore = () => score;
+    const increaseScore = () => score++;
+
+
+    return { playerName, playerMarker }
+}
+
+  
+
+//Use gameboard to keep track of markers
+
+let gameboard = createGameboard();
+
+function createGameboard() {
+    let gameboard = [];
+    fillGameboardWithCells();
+
+    function createCell(id) {
+        let cellId = id;
+        let cellMarker = "";
+        return { cellId, cellMarker }
+    }
+
+    function fillGameboardWithCells() {
+        for (i=0;i<9;i++) {
+            gameboard[i] = createCell(i);
+        }
+    }
+
+    return gameboard;
+}
+
+// Display controller to initiate interface
+const displayController = (function() {
+    
+    const startGameButton = document.querySelector(".startGameButton");
+    const tableGameboard = document.querySelector(".tableGameboard");
+    const announcement = document.querySelector(".announcement");
+    const gameboardCell = document.querySelectorAll(".gameboardCell");
+
+    gameboardCell.forEach( cell => {
+        cell.addEventListener("click", (e) => {
+            gameController.playRound(parseInt(e.target.dataset.index),gameController.getCurrentPlayer());
+            
+            for (i=0;i<gameboardCell.length;i++) {
+                    console.log(`updated!`)
+                    gameboardCell[i].textContent = gameboard[i].cellMarker; 
+            }
+            
+        })
+    })
+
+    startGameButton.addEventListener("click", () => {
+        startGameButton.style.display = "none";
+    })
+
+    const updateGameboard = () => {
+        for (i=0;i<gameboardCell.length;i++) {
+            console.log(`updated!`)
+            gameboardCell.textContent = gameboard[i].cellMarker;
+        }
+    }
+
+    return { announcement, gameboardCell }
+})();
+
+//Use gameController to facilitate gameplay
+
+const gameController = (function () {
+    const player1 = createPlayer("player1", "X");
+    const player2 = createPlayer("player2", "O");
+    
+    let turn = 0;
+    let gameOver = false;
+
+    const playRound = (number,currentPlayer) => {
+
+        if (turn === 9) {
+            displayController.announcement.textContent(`It's a draw`);
+            gameOver = true;
+            return;
+          }
+
+        let selectedCell = gameboard[number];
+        console.log(`The selected cell is currently marked as ${selectedCell.cellMarker}`)
+
+        if (selectedCell.cellMarker == "") {
+            console.log(`Marking cell with ${currentPlayer.playerMarker}`)
+            gameboard[number].cellMarker = currentPlayer.playerMarker;
+            console.table(gameboard);
+            checkWinCondition(currentPlayer);
+            turn++;
+        } else {
+            console.log(`This cell is already occupied.`)
+        }
+        
+    }
+
+    const getCurrentPlayer = () => {
+        return turn%2 === 0 ? player1 : player2
+    }
+
+    function checkWinCondition(player) {
+        let x = player.playerMarker;
+        
+        if (gameboard[0].cellMarker == x && 
+            gameboard[1].cellMarker == x && 
+            gameboard[2].cellMarker == x) {
+                return announcePlayerWin(player)
+        }
+
+        if (gameboard[3].cellMarker == x && 
+            gameboard[4].cellMarker == x && 
+            gameboard[5].cellMarker == x) {
+                return announcePlayerWin(player)
+        }
+
+        if (gameboard[6].cellMarker == x && 
+            gameboard[7].cellMarker == x && 
+            gameboard[8].cellMarker == x) {
+                return announcePlayerWin(player)
+        }
+
+        if (gameboard[0].cellMarker == x && 
+            gameboard[3].cellMarker == x && 
+            gameboard[6].cellMarker == x) {
+                return announcePlayerWin(player)
+        }
+
+        if (gameboard[1].cellMarker == x && 
+            gameboard[4].cellMarker == x && 
+            gameboard[7].cellMarker == x) {
+                return announcePlayerWin(player)
+        }
+
+        if (gameboard[2].cellMarker == x && 
+            gameboard[5].cellMarker == x && 
+            gameboard[8].cellMarker == x) {
+                return announcePlayerWin(player)
+        }
+
+        if (gameboard[0].cellMarker == x && 
+            gameboard[4].cellMarker == x && 
+            gameboard[8].cellMarker == x) {
+                return announcePlayerWin(player)
+        }
+
+        if (gameboard[6].cellMarker == x && 
+            gameboard[4].cellMarker == x && 
+            gameboard[2].cellMarker == x) {
+                return announcePlayerWin(player)
+        }
+    }
+
+    function checkTieCondition() {
+        let occupiedSpace = 0;
+        let tieCondition = false;
+
+        for (i=0;i<9;i++) {
+            if(gameboard[i].cellMarker!=='') {
+                occupiedSpace++;
+            }
+        }
+        if (occupiedSpace==9) {
+            tieCondition = true;
+            announceTie();
+        } else {
+            occupiedSpace = 0;
+            increaseTurn();
+            playerTurn();
+        }
+
+        return tieCondition
+    }
+
+    function announcePlayerWin(player) {
+        alert(`${player.playerName} has won!`)
+
+    }
+
+    function announceTie() {
+        alert(`It's a draw!`)
+    }
+
+    return { playRound, checkTieCondition, getCurrentPlayer }
+})();
